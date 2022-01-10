6bce8c47b48d363a262153faac1d5dca1d1947fb
Hanake0 <bghaesee@outlook.com>
2022-01-10 10:47:26 -0300

    Fix(Duping): Fixed some bugs related to players being able to edit the guis and/or dupe itens

--- src/main/java/dev/gump/mars/inventory/InventoryListener.java
+++ src/main/java/dev/gump/mars/inventory/InventoryListener.java
@@ -2,19 +2,16 @@ package dev.gump.mars.inventory;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class InventoryListener implements Listener {
     public static List<Inventory> itemsCanOut = new ArrayList<>();
-
     public static HashMap<Inventory, HashMap<ItemStack, GUIClickEvent>> itemActions = new HashMap<>();
     public static HashMap<Inventory,HashMap<Integer, GUIClickEvent>> slotActions = new HashMap<>();
     public static HashMap<Inventory,GUICloseEvent> closedActions = new HashMap<>();
@@ -24,30 +21,62 @@ public class InventoryListener implements Listener {
     }
 
     @EventHandler
-    public void OnClickItemInventory(InventoryClickEvent event){
-        if(itemActions.containsKey(event.getClickedInventory()) && itemActions.get(event.getClickedInventory()).containsKey(event.getCurrentItem()))
-            itemActions.get(event.getClickedInventory()).get(event.getCurrentItem()).onClick(event);
+    public void OnInventoryClick(InventoryClickEvent event){
+        Inventory inv = event.getClickedInventory();
+        Inventory targetInv = event.getInventory();
+        InventoryAction action = event.getAction();
 
-        if(slotActions.containsKey(event.getClickedInventory()) && slotActions.get(event.getClickedInventory()).containsKey(event.getSlot()))
-            slotActions.get(event.getClickedInventory()).get(event.getSlot()).onClick(event);
+        // Item actions
+        if(itemActions.containsKey(inv) && itemActions.get(inv).containsKey(event.getCurrentItem()))
+            itemActions.get(inv).get(event.getCurrentItem()).onClick(event);
 
-        if(itemsCanOut.contains(event.getClickedInventory()))
+        // Slot actions
+        if(slotActions.containsKey(inv) && slotActions.get(inv).containsKey(event.getSlot()))
+            slotActions.get(inv).get(event.getSlot()).onClick(event);
+
+        // If the clicked inventory is a gui
+        if(itemsCanOut.contains(inv))
             event.setCancelled(true);
+
+        // If the recipient inventory is a gui
+        if(itemsCanOut.contains(targetInv)){
+            if (action == InventoryAction.MOVE_TO_OTHER_INVENTORY)
+                event.setCancelled(true);
+
+            if (action == InventoryAction.COLLECT_TO_CURSOR)
+                event.setCancelled(true);
+        }
     }
 
     @EventHandler
-    public void OnCloseInventory(InventoryCloseEvent event){
-        itemsCanOut.remove(event.getInventory());
+    public void OnInventoryDrag(InventoryDragEvent event){
+        InventoryView view = event.getView();
+        Iterator<Integer> it = event.getRawSlots().iterator();
 
-        if(itemActions.containsKey(event.getInventory()))
-            itemActions.remove(event.getInventory(), itemActions.get(event.getInventory()));
+        // If one of the final slots is a gui
+        it.forEachRemaining(i -> {
+            if (itemsCanOut.contains(view.getInventory(i)))
+                event.setCancelled(true);
+        });
+    }
 
-        if(slotActions.containsKey(event.getInventory()))
-            slotActions.remove(event.getInventory(), slotActions.get(event.getInventory()));
+    @EventHandler
+    public void OnInventoryClose(InventoryCloseEvent event){
+        Inventory inv = event.getInventory();
+        itemsCanOut.remove(inv);
 
-        if(closedActions.containsKey(event.getInventory())) {
-            closedActions.get(event.getInventory()).onClose(event);
-            closedActions.remove(event.getInventory(), closedActions.get(event.getInventory()));
+        // Item actions
+        if(itemActions.containsKey(inv))
+            itemActions.remove(inv, itemActions.get(inv));
+
+        // Slot actions
+        if(slotActions.containsKey(inv))
+            slotActions.remove(inv, slotActions.get(inv));
+
+        // Close actions
+        if(closedActions.containsKey(inv)) {
+            closedActions.get(inv).onClose(event);
+            closedActions.remove(inv, closedActions.get(inv));
         }
     }
 }
